d7b535e391383efb05cfe8010c1b659f8e9cf5fc
DR <damjan.ramsak@gmail.com>
2019-11-21 17:21:34 +0100

    Dodal šifriranje gesla

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import hashlib
 import random
 import uuid
 # import Flask knjižnico
@@ -46,14 +46,21 @@ def poslji_sporocilo():
 @app.route("/prijava", methods=["POST"])
 def prijava():
     ime = request.form.get("ime")
+    originalno_geslo = request.form.get("geslo")
+    geslo = hashlib.sha256(originalno_geslo.encode()).hexdigest()
 
     sejna_vrednost = str(uuid.uuid4())
 
-    uporabnik = db.query(Uporabnik).filter_by(sejna_vrednost=sejna_vrednost).first()
+    # Geslo je potrebno šifrirati!
+
+    uporabnik = db.query(Uporabnik).filter_by(ime=ime).first()
     if not uporabnik:
-        uporabnik = Uporabnik(ime=ime, sejna_vrednost=sejna_vrednost)
+        uporabnik = Uporabnik(ime=ime, geslo=geslo, sejna_vrednost=sejna_vrednost)
     else:
-        uporabnik.sejna_vrednost = sejna_vrednost
+        if geslo == uporabnik.geslo:
+            uporabnik.sejna_vrednost = sejna_vrednost
+        else:
+            return "Napačno geslo"
 
     db.add(uporabnik)
     db.commit()
