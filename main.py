395a94a7114e0e58f2f3c1d9886fb66d0e20a0dc
DR <damjan.ramsak@gmail.com>
2019-11-25 17:42:30 +0100

    Dodal izpis vseh uporabnikov

--- main.py
+++ main.py
@@ -170,5 +170,12 @@ def izbrisi_profil():
         odgovor.set_cookie("sejna_vrednost", expires=0)
         return odgovor
 
+@app.route("/uporabniki")
+def uporabniki():
+    users = db.query(Uporabnik).all()
+    return render_template("uporabniki.html", uporabniki=users)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
